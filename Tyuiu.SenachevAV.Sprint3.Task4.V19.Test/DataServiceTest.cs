54a08758c568a7666ab79dab733cfcfd556d1509
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-10-31 23:36:49 +0500

    Task4

---
+++ Tyuiu.SenachevAV.Sprint3.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Tyuiu.SenachevAV.Sprint3.Task4.V19.Lib;
+
+namespace Tyuiu.SenachevAV.Sprint3.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = -0.226;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

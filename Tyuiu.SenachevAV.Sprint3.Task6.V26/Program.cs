f7be95aa25c589c3bdb1c824fef158c8a61d2fad
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-11-01 00:58:18 +0500

    Task6

---
+++ Tyuiu.SenachevAV.Sprint3.Task6.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.SenachevAV.Sprint3.Task6.V26.LIb;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Сеначев А.В. | АСОиУб-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУб-24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Напишите программу, которая ищет среди целых чисел,                     *");
+        Console.WriteLine("* принадлежащих числовому отрезку [15, 22] количество всех делителей      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = 15;
+        int stopValue = 22;
+
+        Console.WriteLine("Старт шага первой суммы ряда= " + startValue);
+        Console.WriteLine("Конец шага первой суммы ряда= " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+
+        Console.WriteLine("Сумма ряда = " + ds.GetSumTheDivisors(startValue, stopValue));
+        Console.ReadKey();
+    }
+}

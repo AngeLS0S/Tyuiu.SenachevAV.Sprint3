26c7b27bfb56e63d5530fcb811322ea9be27924b
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-10-31 21:03:41 +0500

    Task1

---
+++ Tyuiu.SenachevAV.Sprint3.Task1.V20/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.SenachevAV.Sprint3.Task1.V20.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Сеначев А.В. | АСОиУб-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла while                                              *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #20                                                             *");
+        Console.WriteLine("* Выполнил: Сеначев Андрей Вячеславович | АСОиУб-24-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда   *");
+        Console.WriteLine("* по формуле                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = 1;
+        int stopValue = 16;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+
+        Console.WriteLine("Сумма = " + ds.GetSumSeries(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
